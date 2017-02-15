7c257c8b2a34eee1f26b54aca12bd06fe9770030
Giorgi Chumburidze <chumburidzegio@live.com>
2017-02-15 21:46:16 +0100

    test social auth

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
         'client_id' => '1859317274325590',
         'client_secret' => '872336b089e99b7d5324a9e9528d68cc',
-        'redirect' => 'http://localhost',
+        'redirect' => 'http://nixler.pl/goo',
     ],
 
 ];
