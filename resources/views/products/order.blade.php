a3da25b8a247366562783798fb804160aa2b34c3
Giorgi Chumburidze <chumburidzegio@live.com>
2017-06-13 19:49:23 +0000

    Show product on purchase page

--- resources/views/products/order.blade.php
+++ resources/views/products/order.blade.php
@@ -12,6 +12,22 @@ window.city_id = <?php echo old('city_id', auth()->user()->city_id) ? : 0; ?>;
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default _b0 _z013">
                 
+                <div class="_lim _clear _mt5 _mb5 _bb1">
+                    <img src="{{ $product->photo('similar') }}" class="_left _dib _mt5" height="60" width="60">
+                    <div class="_pl15 _pr15 _pb10 _oh">
+                        <a class="_cbt8 _lh1 _mb0 _telipsis _w100 _clear _pr10 _fs18" href="{{ $product->url() }}">
+                            {{ $product->title }}
+                        </a>
+                        <span class="_cbt8 _clear _telipsis _w100 _oh _pr10 _oh _fs13">
+                            By <a href="{{ $product->owner->link() }}" class="_ci">{{ $product->owner->name }}</a>
+                        </span>
+                        <span class="_cbt8 _clear _telipsis _w100 _oh _pr10 _oh _fs13">
+                            {{ $product->price_formated }}
+                        </span>
+                    </div>
+                </div>
+
+
                 <span class="_c2 _lh1 _mb0 _fs18 _p15 _clear _pb0">
                     @lang('Add shipping details')
                     <span ng-if="vm.city.shipping" ng-bind="vm.city.shipping" class="_right _fs14 _cbl ng-cloak"></span>
