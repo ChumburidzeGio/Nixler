f2af8b80a4f0085cfd77234b33dc21e0909b3075
Giorgi Chumburidze <chumburidzegio@live.com>
2017-08-19 23:35:31 +0400

    Correct notifiable path

--- app/Services/SystemService.php
+++ app/Services/SystemService.php
@@ -8,7 +8,7 @@ use Exception, Debugbar;
 class SystemService
 {
     public function notify($notifcation) {
-        app(\App\Notifications\Notifiable\Notifiable::class)->notify($notifcation);
+        app(\App\Notifications\Notifiables\Notifiable::class)->notify($notifcation);
     }
 
     public function reportException($e) {
