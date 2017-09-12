d1fc5474f487811bb5ab7ee5965eb77ac87f24fd
Giorgi Chumburidze <chumburidzegio@live.com>
2017-09-12 18:07:35 +0000

    Add gd to ci

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install dependencies only for Docker.
-[[ ! -e /.dockerinit ]] && exit 0
-set -xe
-
 # Update packages and install composer and PHP dependencies.
 apt-get update -yqq
 
